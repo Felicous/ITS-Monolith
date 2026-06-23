96814ff86f79f27cb904e8457dc053e5402cda85
MSteul <steulm18@gmail.com>
2026-06-23 18:54:36 +0200

    Start of the project with DomainModel.js and ersistence folder

---
+++ src/DomainModel.js
@@ -0,0 +1,20 @@
+/** DomainReader */
+class DomainReader {
+    /**
+     * Ctor for reading the files
+     */
+    constructor() {
+        /**
+        * Member variables
+        * @typedef {Concept[]} concepts - list of Concepts
+        * @typedef {Task[]} tasks - list of tasks
+        */
+        let concepts;
+        let tasks;
+
+        // Code for reading the files
+
+        this._concepts = _concepts
+        this._tasks = _tasks
+    }
+}
