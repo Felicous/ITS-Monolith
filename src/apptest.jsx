269fb4fbd9df3828c989b0f3175f1507469a9447
DanielPodlich <dpodlich42@gmail.com>
2026-07-01 14:32:03 +0200

    modified apptest.jsx

--- src/apptest.jsx
+++ src/apptest.jsx
@@ -1,17 +1,21 @@
 // import { useState } from 'react'
 // import { task } from './task_mockup'
 import { DisplayMultiplechoice } from './DisplayMultiplechoice'
+import { HintDisplay } from './HintDisplay';
 import { TaskSelection } from './TaskSelection'
 
 
 
 function TestApp() {
 
+  const currentTask = TaskSelection();
+
   return (
-    <>
-    <DisplayMultiplechoice task={TaskSelection()}/>
-    </>
-  )
+    <div style={{ maxWidth: '500px', margin: '0 auto', padding: '20px' }}>
+      <DisplayMultiplechoice task={currentTask} />
+      <HintDisplay task={currentTask} />
+    </div>
+  );
 }
 
-export default TestApp
+export default TestApp;
